832b5c9ddcefa4946ae15c7a3a83c47028413554
Siamak Alipour <siamakalipour37@gmail.com>
2021-11-15 16:45:07 +0330

    test

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Main from './container/Main';
+
+function App() {
+	return (
+		<Router>
+			<div className='app'>
+				<Routes>
+					<Route path='/messenger' element={<Main />} />
+					<Route index path='/messenger/login' element={<Login />} />
+					<Route path='/messenger/register' element={<Register />} />
+				</Routes>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
