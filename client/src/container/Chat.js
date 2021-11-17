629c1e9fe6b60f2ec77e1422a03349bf158dd3e2
Siamak Alipour <siamakalipour37@gmail.com>
2021-11-17 12:39:14 +0330

    chat part made

--- client/src/container/Chat.js
+++ client/src/container/Chat.js
@@ -1,9 +1,56 @@
 import React from 'react';
 import './styles/Chat.scss';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { IconButton } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import TelegramIcon from '@mui/icons-material/Telegram';
 function Chat() {
+	const handleClick = () => {
+		let rightPart = document.getElementById('right');
+		let leftPart = document.getElementById('left');
+		let backButton = document.getElementById('backButton');
+		let chatHeader = document.getElementById('chatHeader');
+		// mainPart.style.flexDirection = 'column';
+		rightPart.classList.remove('right__click');
+		leftPart.classList.remove('left__click');
+		backButton.classList.remove('buttonShow');
+		chatHeader.classList.remove('show');
+	};
 	return (
 		<div className='chat'>
-			<div className='chat__header'></div>
+			<div className='chat__header' id='chatHeader'>
+				<div className='chat__headerLeftPart'>
+					{/* backToContacts */}
+					<ArrowBackIosNewIcon
+						id='backButton'
+						onClick={handleClick}
+					/>
+
+					{/* avatar */}
+					<div className='chat__contactAvatar'>
+						<PersonIcon />
+					</div>
+				</div>
+				<div className='chat__headerRightPart'>
+					<h2>Lorem Ipsum</h2>
+				</div>
+			</div>
+			<div className='chat__content'>
+				{/* chat part */}
+				<div className='chat__contentPart'></div>
+				{/* chat input */}
+
+				<form className='chat__contentInput'>
+					<input type='text' className='chat__input' />
+					<button
+						type='submit'
+						onClick={(e) => {
+							e.preventDefault();
+						}}>
+						<TelegramIcon />{' '}
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
