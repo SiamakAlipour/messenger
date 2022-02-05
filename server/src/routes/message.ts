32ce2858c07a18a3a28918dfbb9625cee3af4b9f
SiamakAlipour <siamakalipour37@gmail.com>
2022-02-05 11:26:10 +0330

    message send and sync done

--- server/src/routes/message.ts
+++ server/src/routes/message.ts
@@ -1,34 +1,58 @@
 import express, { Request, Response, Router } from 'express';
-import Chat from '../models/Message';
+import Chat from '../models/Chat';
 const route: Router = express.Router();
 
 route.post('/send', async (req: Request, res: Response) => {
     const { senderName, receiverName, message, timestamp } = req.body;
-
     try {
-        const newMessage = new Chat({
-            users: [senderName, receiverName],
-            chat: [
-                {
-                    receiver: receiverName.toLowerCase(),
-                    message,
-                    timestamp,
+        const sendMessage = await Chat.updateOne(
+            {
+                users: { $all: [senderName, receiverName] },
+            },
+            {
+                $push: {
+                    chat: {
+                        receiver: receiverName.toLowerCase(),
+                        message,
+                        timestamp,
+                    },
                 },
-            ],
-        });
-        await newMessage.save();
-        res.status(201).send(newMessage);
+            }
+        );
+        res.status(200).send(sendMessage);
     } catch (error) {
-        res.status(400).send({ error });
+        res.status(400).send(error);
     }
+
+    // if (!findMessage) {
+    //     try {
+    //         const newMessage = new Chat({
+    //             users: [senderName, receiverName],
+    //             chat: [
+    //                 {
+    //                     receiver: receiverName.toLowerCase(),
+    //                     message,
+    //                     timestamp,
+    //                 },
+    //             ],
+    //         });
+    //         await newMessage.save();
+    //         res.status(201).send(newMessage);
+    //     } catch (error) {
+    //         res.status(400).send({ error });
+    //     }
+    // }
 });
 route.get('/sync', async (req: Request, res: Response) => {
     const { user1, user2 } = req.body;
     try {
         const findMessage = await Chat.findOne({
-            users: { $all: [user1, user2], $size: 2 },
+            users: {
+                $all: [user1.toLowerCase(), user2.toLowerCase()],
+                $size: 2,
+            },
         });
-        res.status(200).json(findMessage);
+        res.status(200).send(findMessage.chat);
     } catch (error) {
         res.status(404).send(error);
     }
