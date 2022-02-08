7a2fe0d2efe8bdcb3494eb3fbb9b64483df32585
SiamakAlipour <siamakalipour37@gmail.com>
2022-02-08 18:59:13 +0330

    register fetch and formik validation done

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import './styles/Register.scss';
-import { Formik } from 'formik';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import './styles/Register.scss'
+import { Field, Form, Formik } from 'formik'
+import { Link } from 'react-router-dom'
+import * as Yup from 'yup'
+import axios from '../service/api/baseUrl'
+import { useNavigate } from 'react-router'
 function Register() {
+	let navigate = useNavigate()
+	const schema = Yup.object().shape({
+		username: Yup.string()
+			.required('Username required')
+			.min(6, 'username should be atleast 6 character'),
+		password: Yup.string()
+			.required('Password required')
+			.min(8, 'password should be 8 character'),
+		repeatPassword: Yup.string().oneOf(
+			[Yup.ref('password'), null],
+			'Passwords must match'
+		),
+		email: Yup.string().email('this is not in email format').required(),
+	})
+	const handleRegister = async (values) => {
+		await axios
+			.post('/account/register', {
+				username: values.username,
+				password: values.password,
+				email: values.email,
+			})
+			.then((res) => {
+				alert('you succesfully registred')
+				navigate('/', { replace: true })
+			})
+			.catch((err) => alert(err))
+	}
 	return (
 		<div className='register'>
 			<div className='register__title'>
@@ -14,12 +44,63 @@ function Register() {
 				<Formik
 					initialValues={{
 						username: '',
+
+						email: '',
+					}}
+					validationSchema={schema}
+					onSubmit={handleRegister}>
+					{({ errors, touched }) => (
+						<Form className='register__form'>
+							<Field name='username' className='form-control register__input' />
+
+							{touched.username && errors.username && (
+								<p className='text-danger'>{errors.username}</p>
+							)}
+							<Field name='password' className='form-control register__input' />
+
+							{touched.password && errors.password && (
+								<p className='text-danger'>{errors.password}</p>
+							)}
+							<Field
+								name='repeatPassword'
+								className='form-control register__input'
+							/>
+
+							{touched.repeatPassword && errors.repeatPassword && (
+								<p className='text-danger'>{errors.repeatPassword}</p>
+							)}
+							<Field name='email' className='form-control register__input' />
+
+							{/* If this field has been touched, and it contains an error, display
+
+           it */}
+
+							{touched.email && errors.email && (
+								<p className='text-danger'>{errors.email}</p>
+							)}
+
+							<button type='submit' className='btn btn-danger'>
+								Submit
+							</button>
+						</Form>
+					)}
+				</Formik>
+				{/* <Formik
+					initialValues={{
+						username: '',
 						email: '',
 						password: '',
 						repeatPassword: '',
+					}}
+					validationSchema={schema}
+					validateOnChange={schema}
+					onSubmit={(values) => {
+						// same shape as initial values
+
+						console.log(values)
 					}}>
 					{({ errors, touched }) => (
-						<form className='register__form'>
+						<Form className='register__form'>
 							<input
 								type='text'
 								className='form-control register__input'
@@ -27,10 +108,8 @@ function Register() {
 								name='username'
 								autoComplete='new-password'
 							/>
-							{errors.username && (
-								<label htmlFor='username'>
-									{errors.username}
-								</label>
+							{errors.username && touched.username && (
+								<label htmlFor='username'>{errors.username}</label>
 							)}
 
 							<input
@@ -40,10 +119,8 @@ function Register() {
 								name='password'
 								autoComplete='new-password'
 							/>
-							{errors.password && (
-								<label htmlFor='password'>
-									{errors.password}
-								</label>
+							{errors.password && touched.password && (
+								<label htmlFor='password'>{errors.password}</label>
 							)}
 							<input
 								type='password'
@@ -52,10 +129,8 @@ function Register() {
 								name='repeatPassword'
 								autoComplete='new-password'
 							/>
-							{errors.repeatPassword && (
-								<label htmlFor='repeatPassword'>
-									{errors.repeatPassword}
-								</label>
+							{errors.repeatPassword && touched.repeatPassword && (
+								<label htmlFor='repeatPassword'>{errors.repeatPassword}</label>
 							)}
 							<input
 								type='email'
@@ -63,19 +138,20 @@ function Register() {
 								placeholder='email'
 								name='email'
 							/>
-							{errors.email && (
+							{errors.email && touched.email && (
 								<label htmlFor='email'>{errors.email}</label>
 							)}
 							<button
 								className='btn btn-danger'
+								type='submit'
 								onClick={(e) => {
-									e.preventDefault();
+									e.preventDefault()
 								}}>
 								Register
 							</button>
-						</form>
+						</Form>
 					)}
-				</Formik>
+				</Formik> */}
 				<div className='register__footer'>
 					<Link to='/account/login'>
 						<h3>sign in</h3>
@@ -83,7 +159,7 @@ function Register() {
 				</div>
 			</div>
 		</div>
-	);
+	)
 }
 
-export default Register;
+export default Register
--- server/src/validation.ts
+++ server/src/validation.ts
@@ -4,7 +4,6 @@ export const registerValidation = (data: object) => {
     const schema = Joi.object({
         username: Joi.string().min(6).max(30),
         password: Joi.string().min(8),
-        repeat_password: Joi.ref('password'),
         email: Joi.string().email(),
         admin: Joi.bool(),
         avatar: Joi.string(),
